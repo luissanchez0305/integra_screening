07a5dff35383119fbf0947f5235654899ccf56ac
Luis Ricardo Sanchez <luis.sanchez@esferasoluciones.com>
2023-12-29 21:30:10 -0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,52 @@
+import ImageFallback from "@/helpers/ImageFallback";
+import { getListPage } from "@/lib/contentParser";
+import { markdownify } from "@/lib/utils/textConverter";
+import CallToAction from "@/partials/CallToAction";
+import SeoMeta from "@/partials/SeoMeta";
+import Testimonials from "@/partials/Testimonials";
+import Button from "@/shortcodes/Button";
+import { Button as ButtonTtype, Feature } from "@/types";
+import DragAndDrop from "@/components/DragAndDrop";
+import Link from "next/link";
+import { FaCheck } from "react-icons/fa/index.js";
+
+const Home = () => {
+  const homepage = getListPage("homepage/_index.md");
+  const testimonial = getListPage("sections/testimonial.md");
+  const callToAction = getListPage("sections/call-to-action.md");
+  const { frontmatter } = homepage;
+  const {
+    banner,
+    features,
+  }: {
+    banner: { title: string; image: string; content?: string; button?: ButtonTtype };
+    features: Feature[];
+  } = frontmatter;
+
+  return (
+    <>
+      <SeoMeta />
+      <section className="section pt-14">
+        <div className="container">
+          <div className="row justify-center">
+            <div className="mb-16 text-center lg:col-7">
+              <h1
+                className="mb-4"
+                dangerouslySetInnerHTML={markdownify(banner.title)}
+              />
+              <p
+                className="mb-8"
+                dangerouslySetInnerHTML={markdownify(banner.content ?? "")}
+              />
+              {banner.button!.enable && (
+                <DragAndDrop label={banner.button!.label}/>
+              )}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/layouts/partials/Header.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import Logo from "@/components/Logo";
+import ThemeSwitcher from "@/components/ThemeSwitcher";
+import config from "@/config/config.json";
+import menu from "@/config/menu.json";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { useEffect } from "react";
+import { IoSearch } from "react-icons/io5/index.js";
+
+//  child navigation link interface
+export interface IChildNavigationLink {
+  name: string;
+  url: string;
+}
+
+// navigation link interface
+export interface INavigationLink {
+  name: string;
+  url: string;
+  hasChildren?: boolean;
+  children?: IChildNavigationLink[];
+}
+
+const Header = () => {
+  // distructuring the main menu from menu object
+  const { main }: { main: INavigationLink[] } = menu;
+  const { navigation_button, settings } = config;
+  // get current path
+  const pathname = usePathname();
+
+  // scroll to top on route change
+  useEffect(() => {
+    window.scroll(0, 0);
+  }, [pathname]);
+
+  return (
+    <header
+      className={`header z-30 ${settings.sticky_header && "sticky top-0"}`}
+    >
+      <nav className="navbar container">
+        {/* logo */}
+        <div className="order-0">
+          <Logo />
+        </div>
+        {/* navbar toggler */}
+        <input id="nav-toggle" type="checkbox" className="hidden" />
+        <label
+          htmlFor="nav-toggle"
+          className="order-3 cursor-pointer flex items-center lg:hidden text-dark dark:text-white lg:order-1"
+        >
+          <svg
+            id="show-button"
+            className="h-6 fill-current block"
+            viewBox="0 0 20 20"
+          >
+            <title>Menu Open</title>
+            <path d="M0 3h20v2H0V3z m0 6h20v2H0V9z m0 6h20v2H0V0z"></path>
+          </svg>
+          <svg
+            id="hide-button"
+            className="h-6 fill-current hidden"
+            viewBox="0 0 20 20"
+          >
+            <title>Menu Close</title>
+            <polygon
+              points="11 9 22 9 22 11 11 11 11 22 9 22 9 11 -2 11 -2 9 9 9 9 -2 11 -2"
+              transform="rotate(45 10 10)"
+            ></polygon>
+          </svg>
+        </label>
+        {/* /navbar toggler */}
+
+        <div className="order-1 ml-auto flex items-center md:order-2 lg:ml-0">
+          {settings.search && (
+            <button
+              className="border-border text-dark hover:text-primary dark:border-darkmode-border mr-5 inline-block border-r pr-5 text-xl dark:text-white dark:hover:text-darkmode-primary"
+              aria-label="search"
+              data-search-trigger
+            >
+              <IoSearch />
+            </button>
+          )}
+          <ThemeSwitcher className="mr-5" />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
